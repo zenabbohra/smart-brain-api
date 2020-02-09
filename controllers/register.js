a71be779dc6f12c290440aa126955899e4a85752
Zenab <zenab.bohra19@gmail.com>
2020-02-09 12:20:47 +1100

    Adds validations for name, email & password and returns responses accordingly

--- controllers/register.js
+++ controllers/register.js
@@ -3,14 +3,26 @@ const handleRegister = (req, res, db, bcrypt) => {
   const { name, email, password } = req.body;
   const hash = bcrypt.hashSync(password, 10);
 
-  if (typeof name !== 'string' || name.length >= 100) {
+  if(!name){
+    return res.status(400).json({err: 'name should not be empty'})
+  } else if (typeof name !== 'string') {
     return res.status(400).json({err: 'name should be a string'});
+  } else if(name.length >= 100) {
+    return res.status(400).json({err: 'name should be less than 100 chars'});
   }
-  if (typeof email !== 'string' || email.length >= 100 || email.length < 5) {
+  if(!email){
+    return res.status(400).json({err: 'email should not be empty'});
+  } else if (typeof email !== 'string') {
     return res.status(400).json({err: 'email should be a string'});
+  }else if(email.length >= 100 || email.length < 5){
+    return res.status(400).json({err: 'email length should be greater than 5'});
   }
-  if (typeof password !== 'string' || password.length > 30 || password.length < 3) {
-    return res.status(400).json({err: 'password should be a string & password length should be greater than 3'});
+  if(!password){
+    return res.status(400).json({err: 'password should not be empty'});
+  } else if (typeof password !== 'string') {
+    return res.status(400).json({err: 'password should be a string'});
+  } else if(password.length > 30 || password.length < 3){
+    return res.status(400).json({err: 'password length should be greater than 3'});
   }
 
   db.transaction(trx => {
